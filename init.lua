cae7017521f85e227e6000c8b4c79c298800dafe
Maxime Rochat <rochat.max@outlook.com>
2024-09-04 23:23:35 +0200

    finished exercises

--- init.lua
+++ init.lua
@@ -695,11 +695,17 @@ require('lazy').setup({
         clangd = {},
         -- gopls = {},
         pyright = {},
+        glasgow = {},
+        wgsl_analyser = {},
         dockerls = {},
         html = {},
         cmake = {},
         cssls = {},
         csharp_ls = {},
+        glsl_analyzer = {
+          filetypes = { 'glsl', 'vert', 'tesc', 'tese', 'frag', 'geom', 'comp', 'html' },
+        },
+
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
